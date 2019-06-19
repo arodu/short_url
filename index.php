afeabc76780cee771157c1895093eeddd5eead30
Alberto Rodriguez <arodu.dev@gmail.com>
2019-06-19 10:48:47 -0400

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	$token = $_GET['token'];
+	unset($_GET['token']);
+
+	if(!empty($token)){
+		include_once('tokens.php');
+
+		if(isset($list[$token])){
+			header("Location: ".$list[$token]."?".http_build_query($_GET));
+			exit();
+		}
+
+	}
+
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+	echo '<h1>404</h1><h3>Not Found.</h3>';
---
+++ tokens.php
@@ -0,0 +1,5 @@
+<?php
+	$list = [
+		'it'=>'http://127.0.0.1/csv_intimador/',
+	];
+?>
